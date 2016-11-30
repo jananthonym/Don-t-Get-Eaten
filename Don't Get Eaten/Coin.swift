4bedd8cf029401121e6b80dd71511e6dafd09e02
Jan <jan.anthony.m@gmail.com>
2016-11-30 10:30:32 -0500

    initial

---
+++ Don't Get Eaten/Coin.swift
@@ -0,0 +1,9 @@
+//
+//  Coin.swift
+//  Don't Get Eaten
+//
+//  Created by Jan Anthony Miranda on 7/19/15.
+//  Copyright (c) 2015 Jan Anthony Miranda. All rights reserved.
+//
+
+import Foundation
---
+++ Don't Get Eaten/Enemy.swift
@@ -0,0 +1,9 @@
+//
+//  Enemy.swift
+//  Don't Get Eaten
+//
+//  Created by Jan Anthony Miranda on 7/19/15.
+//  Copyright (c) 2015 Jan Anthony Miranda. All rights reserved.
+//
+
+import Foundation
